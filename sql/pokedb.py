701d768fcc3379fdfe7c3333b4a9c81980a7ee01
CMahk <68128075+CMahk@users.noreply.github.com>
2020-08-26 23:05:03 -0700

    Have pokedb.py use CSV instead of JSON

--- sql/pokedb.py
+++ sql/pokedb.py
@@ -19,25 +19,31 @@ class PokeDB():
 
 	async def setup(this):
 		# Setup the db schema
-		this.db = await aiosqlite.connect(this.path + '\\poke.db')
+		this.db = await aiosqlite.connect(this.path + "\\poke.db")
 		this.cursor = await this.db.cursor()
-		async with aiofiles.open(this.path + '\\schema.sql', 'r') as schema:
+		async with aiofiles.open(this.path + "\\schema.sql", "r") as schema:
 			await this.cursor.executescript(await schema.read())
 
 		# Setup the db if it isn't already (see if the first row exists)
-		await this.cursor.execute('SELECT * FROM pokemon WHERE dex LIKE ?', '1')
-		check = await this.cursor.fetchone()
-		if check is None:
-			print('Database not found. Attempting to build one')
-			print('Building pokemon table...')
+		await this.cursor.execute("SELECT * FROM pokemon WHERE dex LIKE ?", "1")
+		checkPoke = await this.cursor.fetchone()
+		await this.cursor.execute("SELECT * FROM pokedex WHERE species LIKE ?", "1")
+		checkDex = await this.cursor.fetchone()
+
+		if checkPoke is None and checkDex is None:
+			print("Database not found. Attempting to build one.")
+
+		if checkPoke is None:
+			print("Building pokemon table...")
 			await this.__populatePokemon()
 
-			print('Building pokedex table...')
+		if checkDex is None:
+			print("Building pokedex table...")
 			await this.__populatePokedex()
 
 	async def __loadList(this, file):
 		fileList = []
-		async with aiofiles.open(this.path + '\\src\\' + file, 'r') as infile:
+		async with aiofiles.open(this.path + "\\src\\" + file, "r") as infile:
 			async for line in infile:
 				fileList.append(line.rstrip())
 
@@ -47,34 +53,34 @@ class PokeDB():
 		# The list corresponds to: [species total for the generation, default game version, region, generation]
 		# See Constants.DICT_VERSION_ID for specifics on what default game version is used to get flavor text
 		dictIndex = {
-			1: [151, 'kanto', 1],
-			152: [251, 'johto', 2],
-			252: [386, 'hoenn', 3],
-			387: [493, 'sinnoh', 4],
-			494: [649, 'unova', 5],
-			650: [721, 'kalos', 6],
-			722: [809, 'alola', 7],
-			#TODO: Flavor text for SwSh
-			810: [892, 'galar', 8]
+			1: [151, "kanto", 1],
+			152: [251, "johto", 2],
+			252: [386, "hoenn", 3],
+			387: [493, "sinnoh", 4],
+			494: [649, "unova", 5],
+			650: [721, "kalos", 6],
+			722: [809, "alola", 7],
+			810: [892, "galar", 8]
 			}
 
 		# POKEMON TABLE
 		dexNumber = 1
-		with open(this.path + '\\src\\poke.csv', 'r') as infile:
+		with open(this.path + "\\src\\poke.csv", "r", encoding = "utf-8") as infile:
 			reader = csv.reader(infile, delimiter = ",")
 			# Get the header out of the way
 			header = next(reader)
-			for row in infile:
+			for row in reader:
 				# Parse the CSV data
-				row = row.split(",")
 				rowParsed = []
 				for item in row:
+					# Cast string numbers to ints
 					if (str.isdigit(item)):
 						rowParsed.append(int(item))
+					# Append null if the string is empty
 					elif (item == ""):
 						rowParsed.append(None)
+					# Replace stand-in characters (they exist to not interfere when splitting the row into a list)
 					else:
-						# Replace stand-in characters (they exist to not interfere when splitting the row into a list)
 						item = item.replace(";", ",").replace("@", "'").replace("#", '"')
 						rowParsed.append(item)
 
@@ -101,34 +107,38 @@ class PokeDB():
 
 	async def __populatePokedex(this):
 		# POKEDEX TABLE
-		# Grabbed the data from
-		# https://github.com/veekun/pokedex/blob/master/pokedex/data/csv/pokemon_species_flavor_text.csv
-
 		# Oh boy this is a thick lad
-		async with aiofiles.open(this.path + '\\src\\pokedex.json', mode = 'r', encoding = 'utf-8') as infile:
-			bigFile = json.loads(await infile.read())
-			for key in bigFile:
-				text = re.sub(r'[\n\f]', ' ', key['flavor_text'])
-				data = [key['species_id'], key['version_id'],key['language_id'], text]
+		with open(this.path + "\\src\\pokedex.csv", mode = "r", encoding = "utf-8") as infile:
+			reader = csv.reader(infile, delimiter = ",")
+			# Get the header out of the way
+			header = next(reader)
+			for row in reader:
+				rowParsed = []
+				for item in row:
+					# Cast string numbers to ints
+					if (str.isdigit(item)):
+						rowParsed.append(int(item))
+					else:
+						rowParsed.append(item)
 
-				await this.cursor.execute("INSERT INTO pokedex (species, version, language, text) VALUES (?,?,?,?)", data)
+				await this.cursor.execute("INSERT INTO pokedex (species, version, language, text) VALUES (?,?,?,?)", row)
 
 		await this.db.commit()
 
 	async def getPokemon(this, value):
 		# Search based on species name
 		if isinstance(value, str):
-			await this.cursor.execute('SELECT * FROM pokemon WHERE species LIKE ?', (value,))
+			await this.cursor.execute("SELECT * FROM pokemon WHERE species LIKE ?", (value,))
 
 		# Search based on dex number
 		elif isinstance(value, int):
-			await this.cursor.execute('SELECT * FROM pokemon WHERE dex LIKE ?', (str(value),))
+			await this.cursor.execute("SELECT * FROM pokemon WHERE dex LIKE ?", (str(value),))
 
 		return await this.cursor.fetchone()
 
 	async def getPokedex(this, value, version, language = 9):
 		if isinstance(value, str):
-			await this.cursor.execute('SELECT * FROM pokemon WHERE species LIKE ?', (value,))
+			await this.cursor.execute("SELECT * FROM pokemon WHERE species LIKE ?", (value,))
 
 			# Get the species' dex number
 			info = await this.cursor.fetchone()
@@ -137,7 +147,7 @@ class PokeDB():
 			version = info[2]
 
 		# 9 = English language
-		await this.cursor.execute('SELECT * FROM pokedex WHERE species LIKE ? AND version LIKE ? AND language LIKE ?', (value, version, language))
+		await this.cursor.execute("SELECT * FROM pokedex WHERE species LIKE ? AND version LIKE ? AND language LIKE ?", (value, version, language))
 		try:
 			result = await this.cursor.fetchone()
 			result = list(result)
@@ -145,4 +155,4 @@ class PokeDB():
 			# Return the flavor text
 			return result[4]
 		except:
-			return ''
+			return ""
